be8a831fadd933eb42c32ee421bae519cbd3aaf3
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-07-06 20:23:36 -0300

    New Category using POST, Seaching Categories using GET

--- src/main/java/com/rlsp/moneyapi/resource/CategoriaResource.java
+++ src/main/java/com/rlsp/moneyapi/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.rlsp.moneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rlsp.moneyapi.model.Categoria;
 import com.rlsp.moneyapi.repository.CategoriaRepository;
@@ -42,6 +51,71 @@ public class CategoriaResource {
 	public List<Categoria> listar(){
 		List<Categoria> categorias = categoriaRepository.findAll();
 		return categorias;
+	}
+	
+	/**
+	 * @RequestBody ==> pega o que FOI ENVIADO para o servidor
+	 * -  O código ideal para resposta de armazenamento no DB é do 201 (created)
+	 * 
+	 * @ResponseStatus(value = HttpStatus.CREATED) ==> apos criado retornara com um 201 (created)
+	 * 
+	 * - O REST pede um location no retorno, mostrando como deve ser recuperado o recurso no futuro
+	 */
+	
+	/**
+	 * Utilizando "findOne"
+	 * 
+	 *  @GetMapping("/{codigo}")
+	 *	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+	 *  	Categoria categoriaExample = new Categoria();
+     * 		categoriaExample.setCodigo(codigo);
+     * 
+     * 		Example<Categoria> example = Example.of(categoriaExample);
+     *
+     * 		return this.categoriaRepository.findOne(example).orElse(null);
+	 * 
+	 */
+	
+	/**
+	 * Utilizando "findById"
+	 * 	@GetMapping("/{codigo}")
+	 *	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+  	 *		return this.categoriaRepository.findById(codigo).orElse(null);
+	 *	}
+	 */
+	
+	// @ResponseStatus(value = HttpStatus.CREATED) == ResponseEntity.created(uri).body(categoriaSalva);
+	@PostMapping
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		// Construindo a LOCATION		
+		URI uri = ServletUriComponentsBuilder
+						.fromCurrentRequestUri()    // pega a URI atual
+						.path("/{codigo}")
+						.buildAndExpand(categoriaSalva.getCodigo()) // Pega o codigo
+						.toUri();
+		
+		response.setHeader("Location", uri.toASCIIString()); // resposta cm a Location
+		
+		// Devolvendo a categoria criada ou salva
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		
+		// Utilizando MAP
+		return this.categoriaRepository.findById(codigo)
+			      .map(categoria -> ResponseEntity.ok(categoria))
+			      .orElse(ResponseEntity.notFound().build());
+		
+		//Utilizando isPresent()
+		/* Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
+		    return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+		*/
+		
 	}
 
 }
