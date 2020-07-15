f9869c8c3b92ee9eecf150b00adb6309b56180de
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-07-15 16:50:21 -0300

    Some changes

--- src/main/java/com/rlsp/moneyapi/RlspmoneyApiApplication.java
+++ src/main/java/com/rlsp/moneyapi/RlspmoneyApiApplication.java
@@ -3,6 +3,7 @@ package com.rlsp.moneyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 import com.rlsp.moneyapi.config.property.RlspMoneyApiProperty;
 
@@ -15,9 +16,15 @@ import com.rlsp.moneyapi.config.property.RlspMoneyApiProperty;
 @EnableConfigurationProperties(RlspMoneyApiProperty.class) // Controle o ambiente de PRoducao
 @SpringBootApplication // Mostra que é uma aplicacao Spring Boot
 public class RlspmoneyApiApplication {
+	
+	private static ApplicationContext APPLICATION_CONTEXT;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RlspmoneyApiApplication.class, args);
+	}
+	
+	public static <T> T getBean(Class<T> type) {
+		return APPLICATION_CONTEXT.getBean(type);
 	}
 
 }
